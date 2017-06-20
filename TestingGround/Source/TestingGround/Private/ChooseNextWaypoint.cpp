f6cb948988fa91dda4793dc1c52b3bbe7d8faa9e
Kim K Baardsen <kkbaardsen@gmail.com>
2017-06-20 14:54:14 +0200

    Lecture 217: Reading Blackboard Data in C++ Added BlackboardComponent to ChooseNextWaypoint to access the Blackboard Key Selector

--- TestingGround/Source/TestingGround/Private/ChooseNextWaypoint.cpp
+++ TestingGround/Source/TestingGround/Private/ChooseNextWaypoint.cpp
@@ -1,11 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// 13th_Panic 2017
 
-#include "ChooseNextWaypoint.h"
+#include "Public/ChooseNextWaypoint.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
 #include "TestingGround.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	
 	//TODO Remove Log
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++ - yay!!"));
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+	
 	return EBTNodeResult::Succeeded;
 }
--- TestingGround/Source/TestingGround/Public/ChooseNextWaypoint.h
+++ TestingGround/Source/TestingGround/Public/ChooseNextWaypoint.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// 13th_Panic 2017
 
 #pragma once
 
@@ -15,10 +15,12 @@ class TESTINGGROUND_API UChooseNextWaypoint : public UBTTaskNode
 {
 	GENERATED_BODY()
 	
-public:
+
 	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
-	
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey;
 };
 
 
