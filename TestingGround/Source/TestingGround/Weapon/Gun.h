43cc03aff7715a0d135b9944fa68a52f5726b876
Kim K Baardsen <kkbaardsen@gmail.com>
2017-06-22 21:40:30 +0200

    Lecture 249: Refactoring to a Gun Actor Refactored all the C++ code for the Gun in First Person Charater to a C++ class Gun. Removed all VR input. Created a Blueprint Based on Gun C++

---
+++ TestingGround/Source/TestingGround/Weapon/Gun.h
@@ -0,0 +1,56 @@
+// 13th_Panic 2017
+
+#pragma once
+
+#include "Engine.h"
+#include "GameFramework/Actor.h"
+#include "Gun.generated.h"
+
+class AProjectile;
+
+UCLASS()
+class TESTINGGROUND_API AGun : public AActor
+{
+	GENERATED_BODY()
+
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USkeletalMeshComponent* FP_Gun;
+	
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	class USceneComponent* FP_MuzzleLocation;
+
+public:	
+	// Sets default values for this actor's properties
+	AGun();
+
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	TSubclassOf<class AProjectile> ProjectileClass;
+	
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class USoundBase* FireSound;
+	
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class UAnimMontage* FireAnimation;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	class UAnimInstance* AnimInstance;
+
+protected:
+	// Called when the game starts or when spawned
+	void BeginPlay() override;
+
+	/** Fires a projectile. */
+	void OnFire();
+
+
+public:	
+	// Called every frame
+	void Tick(float DeltaTime) override;
+
+		
+};
